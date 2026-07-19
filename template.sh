2c0352f660d3fa936a134282f45def21c1efc9bb
yashwitha <yashwithaslg007@gmail.com>
2026-07-19 09:51:10 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# creating folders
+mkdir -p src
+mkdir -p research
+# creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipnb
+touch requirements.txt
+
+
+echo "files and folders are created."
