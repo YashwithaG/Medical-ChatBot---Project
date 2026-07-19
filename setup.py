6524cdc4cdbf44fe70ae5bc84c85f7c7feae2a8b
yashwitha <yashwithaslg007@gmail.com>
2026-07-19 11:41:24 +0530

    requirements added succesfully

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="Medical-Chatbot",
+    version="0.1.0",
+    author="Yahwitha",
+    author_email="yashwithaslg007@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
