b1dceb0d737c225de40769299731fe96d84ed90c
yashwitha <yashwithaslg007@gmail.com>
2026-08-01 23:56:41 +0530

    finalchanges

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ prompt = ChatPromptTemplate.from_messages(
 )
 
 question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
+
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
 
